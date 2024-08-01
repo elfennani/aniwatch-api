6c033fad3a005f1575fb5642379cbd591ba391b8
Nizar Elfennani <elfennani2002@gmail.com>
2024-08-01 18:58:41 +0100

    reversed changes

--- src/serverless.ts
+++ src/serverless.ts
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../src/app.js"));
+app.register(import("./app.js"));
 
 export default async (req: any, res: any) => {
   await app.ready();
