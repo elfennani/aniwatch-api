e1860000274118bec98c30fa2899dfb59d2c2428
Nizar Elfennani <elfennani2002@gmail.com>
2024-08-01 18:56:14 +0100

    fix vercel

--- api/serverless.ts
+++ api/serverless.ts
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("./app.js"));
+app.register(import("../src/app.js"));
 
 export default async (req: any, res: any) => {
   await app.ready();
