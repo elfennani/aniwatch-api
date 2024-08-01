4f55eb1a883fd69ffeb57f5eb69e42aefe4e9ae9
Nizar Elfennani <elfennani2002@gmail.com>
2024-08-01 15:47:06 +0100

    Now episodes use titles provided by AniList if they exist

--- src/routes/show/index.ts
+++ src/routes/show/index.ts
@@ -1,77 +1,69 @@
-import { FastifyPluginAsync } from "fastify"
-import { graphql } from "gql.tada"
-import { z } from "zod"
-import { ShowDetails } from "../../models/show-details.js"
-import { mapAniListTag } from "../../models/tag.js"
-import { reverseMapStatus } from "../../models/status.js"
-import { Episode } from "../../models/episode.js"
-import { retry } from "ts-retry-promise"
+import { FastifyPluginAsync } from "fastify";
+import { graphql } from "gql.tada";
+import { z } from "zod";
+import { ShowDetails } from "../../models/show-details.js";
+import { mapAniListTag } from "../../models/tag.js";
+import { reverseMapStatus } from "../../models/status.js";
+import { Episode } from "../../models/episode.js";
+import { retry } from "ts-retry-promise";
 
 const paramsSchema = z.object({
   id: z.coerce.number(),
-})
+});
 
 const showByID: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/:id', async function (request, reply): Promise<ShowDetails> {
-    const { id } = request.validate(paramsSchema, 'params');
+  fastify.get("/:id", async function (request, reply): Promise<ShowDetails> {
+    const { id } = request.validate(paramsSchema, "params");
     const client = request.getAnilistClient();
     const allanime = fastify.getAllAnimeClient();
 
     const { media } = await client.request(media_query, { id });
 
-    const search = media?.title?.userPreferred
+    const search = media?.title?.userPreferred;
     let episodes: Episode[] = [];
     let showId: string | undefined;
 
     const showSearch: ShowQuery = await retry(
       () => allanime.request(show_query, { search }),
       { retries: 3 }
-    )
-    const show = showSearch!!.shows.edges?.find(
-      (show) => show.aniListId == id
     );
+    const show = showSearch!!.shows.edges?.find((show) => show.aniListId == id);
 
-    showId = show?._id
+    showId = show?._id;
 
     if (show && showId) {
-      const max = show.availableEpisodesDetail.sub.length
+      const max = show.availableEpisodesDetail.sub.length;
 
       const allAnimeEpisodes: AllAnimeEpisodes = await retry(
         () => allanime.request(episodes_details_query, { showId, max }),
         { retries: 3 }
       );
 
-      episodes = allAnimeEpisodes.episodeInfos.map(ep => ({
-        id: ep._id,
-        allanimeId: showId,
-        animeId: id,
-        episode: ep.episodeIdNum,
-        name: `Ep ${ep.episodeIdNum}`,
-        thumbnail: ep.thumbnails
-          ?.filter((t) => !t.includes("cdnfile"))
-          ?.map(t => t.startsWith("http") ? t : (source + t))?.[0],
-        duration: ep.vidInforssub?.vidDuration,
-        dubbed: show?.availableEpisodesDetail.dub.includes(String(ep.episodeIdNum)) ?? false
-      })) ?? [];
+      episodes = show.availableEpisodesDetail.sub.map((ep) => {
+        const details = allAnimeEpisodes.episodeInfos.find(
+          (info) => info.episodeIdNum.toString() === ep
+        );
+        const anilistDetails = media?.streamingEpisodes?.find((episode) => {
+          return episode?.title?.startsWith(`Episode ${ep} `);
+        });
 
-      episodes = show.availableEpisodesDetail.sub.map(ep => {
-        const details = allAnimeEpisodes.episodeInfos.find(info => info.episodeIdNum.toString() === ep);
-
-        return ({
+        return {
           id: details?._id ?? `EP-${ep}`,
           allanimeId: showId,
           animeId: id,
           episode: details?.episodeIdNum ?? Number(ep),
-          name: `Ep ${ep}`,
-          thumbnail: details?.thumbnails
-            ?.filter((t) => !t.includes("cdnfile"))
-            ?.map(t => t.startsWith("http") ? t : (source + t))?.[0],
+          name: anilistDetails?.title ?? `Episode ${ep}`,
+          thumbnail:
+            anilistDetails?.thumbnail ??
+            details?.thumbnails
+              ?.filter((t) => !t.includes("cdnfile"))
+              ?.map((t) => (t.startsWith("http") ? t : source + t))?.[0],
           duration: details?.vidInforssub?.vidDuration,
-          dubbed: show.availableEpisodesDetail.dub.includes(String(ep)) ?? false
-        })
-      })
+          dubbed:
+            show.availableEpisodesDetail.dub.includes(String(ep)) ?? false,
+        };
+      });
     }
-
 
     return {
       id: media?.id!,
@@ -92,17 +84,17 @@ const showByID: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
         large: media?.coverImage?.large!,
         medium: media?.coverImage?.medium!,
         original: media?.coverImage?.extraLarge!,
-        color: media?.coverImage?.color || undefined
-      }
-    }
-  })
-}
+        color: media?.coverImage?.color || undefined,
+      },
+    };
+  });
+};
 
-export default showByID
+export default showByID;
 
 const media_query = graphql(`
   query GetMedia($id: Int) {
-    media:Media(id: $id) {
+    media: Media(id: $id) {
       id
       genres
       episodes
@@ -120,11 +112,11 @@ const media_query = graphql(`
         large
         color
       }
-      mediaListEntry{
+      mediaListEntry {
         progress
         status
       }
-      tags{
+      tags {
         id
         name
         description
@@ -132,12 +124,16 @@ const media_query = graphql(`
         isMediaSpoiler
         isGeneralSpoiler
       }
+      streamingEpisodes {
+        title
+        thumbnail
+      }
     }
   }
 `);
 
 // Thumbnail source for the ones without domain in them
-const source = "https://wp.youtube-anime.com/aln.youtube-anime.com"
+const source = "https://wp.youtube-anime.com/aln.youtube-anime.com";
 
 interface ShowQuery {
   shows: {
@@ -154,7 +150,6 @@ interface ShowQuery {
     ];
   };
 }
-
 
 const show_query = `
   query($search: String) {
